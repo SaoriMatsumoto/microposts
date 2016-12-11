8f6a7a6e692cb6088c3aa038233c706dde30c6cf
Saori Matsumoto <garnerion@gmail.com>
2016-12-11 03:10:14 +0000

    followings-followers challenge

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,6 +33,18 @@ class UsersController < ApplicationController
     end
   end
   
+  def followings
+    @user = User.find(params[:id])
+    @users = @user.following_users
+    render 'show_followings'
+  end
+  
+  def followers
+    @user = User.find(params[:id])
+    @users = @user.follower_users
+    render 'show_followers'
+  end
+
   private
   
   def user_params
